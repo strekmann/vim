6f7aa8cf1ebbf74fc693ae27e2f398a28c56c729
Sigurd Gartmann <sigurdga@sigurdga.no>
2017-04-11 12:10:52 +0200

    Add explanation of what to do next

--- setup.sh
+++ setup.sh
@@ -16,3 +16,12 @@ fi
 vim +BundleInstall +BundleClean +qall
 
 ~/.vim/bundle/fonts/install.sh
+
+cat << EOF
+Now, make sure to add these lines to your .zshrc or .bashrc:
+
+BASE16_SHELL=$HOME/.config/base16-shell/
+[ -n "\$PS1" ] && [ -s \$BASE16_SHELL/profile_helper.sh ] && eval "\$(\$BASE16_SHELL/profile_helper.sh)"
+
+And then run base16_* (tab completion should work) to set color scheme
+EOF
